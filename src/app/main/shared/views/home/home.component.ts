d33498976ff794768ca66d912c7049492640b46c
Pedro Henrique Amaral Souza <pedro.henrique@bancointer.com.br>
2020-08-06 14:48:46 -0300

    A

--- src/app/main/shared/views/home/home.component.ts
+++ src/app/main/shared/views/home/home.component.ts
@@ -2,7 +2,6 @@ import { AuthService } from './../../../../auth/services/auth.service';
 import { UsuarioResponse } from './../../../../auth/models/usuario-response.model';
 import { Component, OnInit } from '@angular/core';
 import { PaymentMethods } from '../../model/payment-methods.model';
-import { NumberValueAccessor } from '@angular/forms';
 
 declare var $: any;
 declare var PagSeguroDirectPayment: any;
@@ -17,13 +16,20 @@ export class HomeComponent implements OnInit {
   usuarioLogado: UsuarioResponse;
   meiosDePagamento: PaymentMethods;
   exibirQuantidadeParcelas: boolean = false;
+  
+  // Dados input pagamento
+  numeroCartao: string;
+  bandeiraSelecionada: string;
+  cvv: string;
+  mesExpiracao: string;
+  anoExpiracao: string;
+
 
   constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
     $('[data-widget="treeview"]').Treeview('init');
-    PagSeguroDirectPayment.setSessionId('3d03d5c109b140dbafaf3382f89dd120');
-    this.getMeiosDePagamento();
+    PagSeguroDirectPayment.setSessionId('94e54b066ab749099370d3acef4d9ad7');
 
   }
 
@@ -36,48 +42,46 @@ export class HomeComponent implements OnInit {
       },
       error: function (response) {
         console.log("Erro ao buscar meios de pagamento: " + response)
-      },
-      complete: function (response) {
-        // Callback para todas chamadas.
       }
     });
-
   }
 
-  // Pegar a bandeira do cartão
+ 
   verificaBandeiraCartao(event: any) {
     var numeroCartao = event.target.value;
     var qtdCaracteres = numeroCartao.length;
 
     if (qtdCaracteres >= 6) {
       this.exibirQuantidadeParcelas = true;
+
+       // Busca a bandeira do cartão digitado
       PagSeguroDirectPayment.getBrand({
         cardBin: numeroCartao,
         success: function (response) {
           var bandeira = response.brand.name;
+          $ ('#bandeiraSelecionadaHidden').val(bandeira)
           $(' .bandeiraCartao').html("<img src=https://stc.pagseguro.uol.com.br/public/img/payment-methods-flags/42x20/" + bandeira + ".png>");
+         
 
-          // Exibe a quantidade e valores das parcelas
+          // Busca a quantidade e valores das parcelas
           PagSeguroDirectPayment.getInstallments({
             amount: 118.80,
             maxInstallmentNoInterest: 2,
             brand: bandeira,
             success: function (response) {
-
+              $('#qtdParcelas').empty();
+              $('#qtdParcelas').append("<option>Selecione</option>")
               $.each(response.installments, function (i, obj) {
                 $.each(obj, function (i2, obj2) {
                   var numberValue = obj2.installmentAmount;
                   var number = "R$ " + numberValue.toFixed(2).replace(".", ",");
-                    $('#qtdParcelas') .append("<option value="+ obj2.installmentAmount+ "> "+ obj2.quantity + " parcelas de "+number+ " </option>")
+                  $('#qtdParcelas').append("<option value=" + obj2.installmentAmount + "> " + obj2.quantity + " parcelas de " + number + " </option>")
                 });
-               
+
               });
             },
             error: function (response) {
-              // callback para chamadas que falharam.
-            },
-            complete: function (response) {
-              // Callback para todas chamadas.
+              console.log("Erro ao buscar valores das parcelas.")
             }
           });
 
@@ -94,23 +98,44 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  // Exibe a quantidade e valores das parcelas
+  createCardToken(){
 
-  getParcelas(bandeira: string) {
-    console.log('entrei')
-    PagSeguroDirectPayment.getInstallments({
-      amount: 118.80,
-      maxInstallmentNoInterest: 2,
-      brand: bandeira,
-      success: function (response) {
+    PagSeguroDirectPayment.createCardToken({
+      cardNumber: this.numeroCartao, // Número do cartão de crédito
+      brand:  $ ('#bandeiraSelecionadaHidden').val(), // Bandeira do cartão
+      cvv: this.cvv, // CVV do cartão
+      expirationMonth: this.mesExpiracao, // Mês da expiração do cartão
+      expirationYear: this.anoExpiracao, // Ano da expiração do cartão, é necessário os 4 dígitos.
+      success: function(response) {
+       $ ('#cardTokenHidden').val(response.card.token)
+      },
+      error: function(response) {
         console.log(response)
-      },
-      error: function (response) {
-        // callback para chamadas que falharam.
-      },
-      complete: function (response) {
-        // Callback para todas chamadas.
       }
-    });
+   });
   }
+
+  getSenderHash(){
+    PagSeguroDirectPayment.onSenderHashReady(function(response){
+      if(response.status == 'error') {
+          console.log(response.message);
+          return false;
+      }
+      var hash = response.senderHash; //Hash estará disponível nesta variável.
+      console.log(hash)
+      $('#senderHashHidden').val(hash);
+  });
+  }
+
+  pagar(){
+    this.createCardToken();
+    this.getSenderHash();
+    console.log(this.numeroCartao);
+    console.log( $ ('#bandeiraSelecionadaHidden').val());
+    console.log(this.cvv);
+    console.log(this.mesExpiracao);
+    console.log(this.anoExpiracao);
+  }
+
+
 }
